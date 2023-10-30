0e2b73cc5f78f44747ac24f7315d810791cba0f7
Juan Plaza <jufeplasa@hotmail.com>
2023-10-30 16:30:39 -0500

    fix(settings): delete coma

--- p1sisworkshop/settings.py
+++ p1sisworkshop/settings.py
@@ -129,5 +129,5 @@ LOGIN_URL = '/signin'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
